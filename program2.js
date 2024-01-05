861cb0d4bac2a949f35840550d374b1264aeba6e
suhani-yadav <96808266+suhani-yadav@users.noreply.github.com>
2024-01-05 00:46:10 -0800

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,40 @@
+
+
+module.exports={romanToInt}
+
 /**
  * @param {string} s
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const currentSymbol = s[i];
+        const currentValue = romanValues[currentSymbol];
+        const nextSymbol = s[i + 1];
+        const nextValue = romanValues[nextSymbol];
+
+        if (nextValue > currentValue) {
+            result += (nextValue - currentValue);
+            
+            i++; // Skip the next symbol since it has been considered
+        } else {
+            result += currentValue;
+        }
+    }
+
+    return result;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
